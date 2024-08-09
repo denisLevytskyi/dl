348ad8a7b6f3dd0c7aec1657846694d7b0b9ba5d
denisLevytskyi <116975058+denisLevytskyi@users.noreply.github.com>
2024-08-09 22:33:27 +0300

    CHANGE PRELOAD TIME

--- data/preload.bat
+++ data/preload.bat
@@ -6,7 +6,7 @@ set aimp=%~dp0AIMP\AIMP.exe
 rmdir /s /q %~dp0AIMP\Profile >nul 2>&1
 start "" "%aimp%"
 
-timeout /t 15
+timeout /t 30
 
 taskkill /Im AIMP.exe /f /T >nul 2>&1
 rmdir /s /q %~dp0AIMP\Profile >nul 2>&1
