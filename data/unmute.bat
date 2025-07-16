d9e3e305af8b9400303f30d7a2ccd6c07113868b
Denis on NewMob <er9p21873@gmail.com>
2025-07-16 12:09:37 +0300

    VERSION 2.4

--- data/unmute.bat
+++ data/unmute.bat
@@ -6,17 +6,22 @@ rem %2 - поток
 rem %3 - плеер
 rem %4 - длительность воспроизведения
 
+set /p cursor_block=<%~dp0var_cursor_block.txt
 set nircmd=%~dp0NIRCMD\NIRCMD.exe
 
-echo ====================
-echo YOUR CURSOR IS BLOCKED...
+if "%cursor_block%"=="1" (
+    echo ====================
+    echo YOUR CURSOR IS BLOCKED...
+)
 %1 /Mute "%2"
 %1 /SetVolume "%2" 0
 
 setlocal enabledelayedexpansion
 for /l %%i in (1, 1, %4) do (
+    if "%cursor_block%"=="1" (
+        start "" %nircmd% setcursor %%i %%i
+    )
     timeout /t 1 >nul 2>&1
-    %nircmd% setcursor %%i %%i
     %1 /Unmute "%3"
     %1 /SetVolume "%3" 100
     echo %%i / %4
